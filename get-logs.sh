538c5cecac91f1c82a939af40a4e3d7c3d7bd9aa
YousraMashkoor <yousra.mashkoor@gmail.com>
2021-03-03 19:29:32 +0500

    Cleaning up

--- get-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-awslocal lambda invoke --function-name my-function --payload '{"key": "value"}' out
-sed -i'' -e 's/"//g' out
-sleep 15
-awslocal logs get-log-events --log-group-name /aws/lambda/my-function --log-stream-name $(cat out) --limit 5
